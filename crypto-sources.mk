2a095834c840752f47db6150ea61cd95232f526a
cpasjuste <cpasjuste@gmail.com>
2016-05-03 17:57:14 +0200

    fixed for android ndk

--- crypto-sources.mk
+++ crypto-sources.mk
@@ -2,11 +2,19 @@ LOCAL_ADDITIONAL_DEPENDENCIES += $(LOCAL_PATH)/sources.mk
 include $(LOCAL_PATH)/sources.mk
 
 LOCAL_CFLAGS += -I$(LOCAL_PATH)/src/include -I$(LOCAL_PATH)/src/crypto -Wno-unused-parameter -std=gnu11
-# Do not add in the architecture-specific files if we don't want to build assembly
-ifeq (,$(filter -DOPENSSL_NO_ASM,$(LOCAL_CFLAGS)))
-LOCAL_SRC_FILES_x86 = $(linux_x86_sources)
-LOCAL_SRC_FILES_x86_64 = $(linux_x86_64_sources)
-LOCAL_SRC_FILES_arm = $(linux_arm_sources)
-LOCAL_SRC_FILES_arm64 = $(linux_aarch64_sources)
-endif
+
 LOCAL_SRC_FILES += $(crypto_sources)
+
+ifeq ($(TARGET_ARCH),arm)
+LOCAL_SRC_FILES += $(linux_arm_sources)
+endif
+ifeq ($(TARGET_ARCH),arm64)
+LOCAL_SRC_FILES += $(linux_aarch64_sources)
+endif
+ifeq ($(TARGET_ARCH),x86)
+LOCAL_SRC_FILES += $(linux_x86_sources)
+endif
+ifeq ($(TARGET_ARCH),x86_64)
+LOCAL_SRC_FILES += $(linux_x86_64_sources)
+endif
+
